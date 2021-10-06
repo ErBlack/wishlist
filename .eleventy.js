6814a9ba57782854b3177b7704fb4ff85ad84f8c
ertema <ertema@yandex-team.ru>
2021-10-06 12:34:05 +0300

    imag

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,28 @@
 const yaml = require('yaml');
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, className) {
+  const {
+    webp: [data]
+  } = await Image(src, {
+    outputDir: 'dist/wishlist/img',
+    widths: [640],
+    formats: ["webp"]
+  });
+
+  return `<img class="${className}" src="/wishlist/${data.url}" alt="${alt}" loading="lazy" decoding="async">`;
+}
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension('yml', contents => yaml.parse(contents));
   eleventyConfig.addPassthroughCopy('wishlist');
+  eleventyConfig.addLiquidFilter("isUrl", (value) => String(value).indexOf('http') === 0);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
 
   return {
     dir: {
       input: "src",
-      output: "build"
+      output: "dist"
     },
     passthroughFileCopy: true
   }
